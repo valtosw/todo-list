aa89e91acf38b21a4d62ea851b91b8c3d1188c86
valtosw <volodijbessarabov@knu.ua>
2025-10-29 19:42:42 +0200

    feat: add Firebase initialization and database setup

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "",
+  authDomain: "",
+  databaseURL:
+    "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
+};
+
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
