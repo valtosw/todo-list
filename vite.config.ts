535b9118fb334fc4b7d00f9a8bf665988ca0e858
valtosw <volodijbessarabov@knu.ua>
2025-11-02 18:11:57 +0200

    Add end-to-end testing coverage with Playwright and Istanbul

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import istanbul from "vite-plugin-istanbul";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    istanbul({
+      include: "src/*",
+      exclude: ["node_modules", "tests/"],
+      extension: [".js", ".ts", ".tsx"],
+      requireEnv: true,
+    }),
+  ],
+});
