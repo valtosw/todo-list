d2921c0f71d18767e6a4ffa24a9cf5c88e0eb769
valtosw <volodijbessarabov@knu.ua>
2025-11-02 17:43:25 +0200

    feat: add Playwright configuration and update dependencies for end-to-end testing

---
+++ playwright.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, devices } from '@playwright/test';
+const port = 5173;
+const baseURL = `http://localhost:${port}`;
+
+export default defineConfig({
+  testDir: './tests',
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: 1,
+  reporter: 'html',
+
+  use: {
+    baseURL,
+    trace: 'on-first-retry',
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+  ],
+
+  webServer: {
+    command: 'npm run dev',
+    url: baseURL,
+    reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
+  },
+});
